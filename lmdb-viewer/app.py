392435602d7cdab644115bfd590bf43ef7cabd27
Ning Lu <jiangxiluning@gmail.com>
2018-08-02 00:06:06 +0800

    修改界面文件，完成初试项目设置

--- lmdb-viewer/app.py
+++ lmdb-viewer/app.py
@@ -1,42 +1,26 @@
 import sys
-from PySide2.QtGui import *
-from PySide2.QtCore import *
+from PySide2.QtWidgets import QApplication
 from PySide2.QtWidgets import QMainWindow
 from views.mainwindow import Ui_MainWindow
 
 
-
 class MainWindow(QMainWindow, Ui_MainWindow):
 
-   def __init__(self):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.assignWidgets()
+        self.show()
 
-       super(MainWindow, self).__init__()
+    def assignWidgets(self):
+        self.pushButton.clicked.connect(self.goPushed)
 
-       self.setupUi(self)
-
-       self.assignWidgets()
-
-       self.show()
-
-   
-
-   def assignWidgets(self):
-
-       self.goButton.clicked.connect(self.goPushed)
-
-   
-
-   def goPushed(self):
-
-       self.goText.append("Go, Go, Go!")
+    def goPushed(self):
+        self.label.setText('done.')
 
 
 if __name__ == '__main__':
-
-   app = QApplication(sys.argv)
-
-   mainWin = MainWindow()
-
-   ret = app.exec_()
-
-   sys.exit( ret )
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    ret = app.exec_()
+    sys.exit(ret)
--- lmdb-viewer/views/mainwindow.py
+++ lmdb-viewer/views/mainwindow.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'mainwindow.ui',
 # licensing of 'mainwindow.ui' applies.
 #
-# Created: Wed Aug  1 22:48:37 2018
+# Created: Wed Aug  1 23:49:13 2018
 #      by: pyside2-uic  running on PySide2 5.11.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,16 +13,51 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 300)
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(919, 580)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.listView = QtWidgets.QListView(self.centralWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listView.sizePolicy().hasHeightForWidth())
+        self.listView.setSizePolicy(sizePolicy)
+        self.listView.setObjectName("listView")
+        self.horizontalLayout.addWidget(self.listView)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem)
+        self.frame = QtWidgets.QFrame(self.centralWidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.horizontalLayout.addWidget(self.frame)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtWidgets.QMenuBar()
+        self.menuBar.setEnabled(True)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 919, 22))
         self.menuBar.setObjectName("menuBar")
         MainWindow.setMenuBar(self.menuBar)
         self.mainToolBar = QtWidgets.QToolBar(MainWindow)
         self.mainToolBar.setObjectName("mainToolBar")
-        MainWindow.addToolBar(self.mainToolBar)
-        self.centralWidget = QtWidgets.QWidget(MainWindow)
-        self.centralWidget.setObjectName("centralWidget")
-        MainWindow.setCentralWidget(self.centralWidget)
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
         self.statusBar = QtWidgets.QStatusBar(MainWindow)
         self.statusBar.setObjectName("statusBar")
         MainWindow.setStatusBar(self.statusBar)
@@ -31,5 +66,7 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "lmdb-viewer", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("MainWindow", "TextLabel", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "PushButton", None, -1))
 
